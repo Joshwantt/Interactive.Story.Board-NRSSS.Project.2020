e015a6884367439b8e3c188b414c8bfa55010025
jbr52 <62874203+jbr52@users.noreply.github.com>
2020-07-24 16:44:04 +1000

    Add files via upload

--- game_objects/gui_button.py
+++ game_objects/gui_button.py
@@ -1,15 +1,14 @@
 import pygame
 import settings
-from graphics.spritesheet import ZoomableSpriteSheet
 from os.path import join
 
 
 class GUIButton(object):
-    def __init__(self, screen, image_path, pos, text, animation_frames, sound_hover=None):
+    def __init__(self, screen, pos, text, sound_hover=None):
         self.selected = False
         self.screen = screen
-        self.spritesheet = ZoomableSpriteSheet(image_path, screen, animation_frames, settings.BUTTON_MIN_ZOOM, settings.BUTTON_ZOOM_RATE, pos[0], pos[1], active=False)
-
+        
+        
         # Only get this sound if it has a sound passed in.
         if sound_hover:
             self.hover_sound = pygame.mixer.Sound(join("assets", "SoundHover", sound_hover))
@@ -19,10 +18,6 @@ class GUIButton(object):
         # Variable for the first frame of being selected.
         self.previously_selected = False
 
-        # Need to get the height of the last frame as it is the largest one.
-        largest = len(self.spritesheet.sprite_sheets) - 1
-        self.button_centre_y = self.spritesheet.sprite_sheets[largest].images[0].get_height()/2
-        
         self.text = text
         self.pos = pos
         
@@ -31,19 +26,14 @@ class GUIButton(object):
 
     def set_pos(self, position):
         self.pos = position
-        self.spritesheet.set_pos(position)
 
     def update(self):
-        # Check for active state of button:
-        if self.selected:
-            self.spritesheet.zoom_in = True
-            self.spritesheet.zoom_out = False
-        else:
-            self.spritesheet.zoom_in = False
-            self.spritesheet.zoom_out = True
-
-        self.spritesheet.update()
         self.draw()
+
+        if self.selected == True:
+            self.font = pygame.font.Font(None, settings.FONT_SIZE + 35)
+        else:
+            self.font = pygame.font.Font(None, settings.FONT_SIZE)
 
         # Check if this is the first frame and if so, play the hover sound.
         if not self.previously_selected and self.selected:
@@ -53,12 +43,11 @@ class GUIButton(object):
     def draw(self):           
         # Draw label.
         text = self.font.render(self.text, True, settings.LABEL_COLOUR)
-        text_rect = text.get_rect(center=(self.pos[0], self.pos[1] + self.button_centre_y + text.get_height()/2))
+        text_rect = text.get_rect(center=(self.pos[0], self.pos[1] + text.get_height()/2))
         self.screen.blit(text, text_rect)
 
     def cleanup(self):
         self.selected = False
-        self.spritesheet.cleanup()
 
     def play_hover_sound(self):
         if self.hover_sound:
